42d243715e15ce6a8d40b40e68148fff709c9220
jingyi <w121748@qq.com>
2026-04-02 12:45:47 +0800

    收发分离client实现

--- client.cpp
+++ client.cpp
@@ -4,9 +4,30 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <string>
-#include <vector>
-#include <mutex>
-#include <algorithm>
+#include <thread>
+void send_messages(int sock){
+    while(true){
+        std::string msg;
+        std::getline(std::cin,msg);
+        if(msg.empty()){
+        continue;
+        }
+        if(msg=="exit"){
+        shutdown(sock,SHUT_WR);
+        break;
+        }
+        send(sock, msg.c_str(),msg.size(), 0);
+    }
+}
+void recv_messages(int sock){
+    while(true){
+        char buffer[1025] ;
+        int n=recv(sock, buffer, 1024, 0);
+        if(n<=0)break;
+        buffer[n] = '\0';
+        std::cout << "\nserver: " << buffer << std::endl;
+    }
+}
 int main() {
 
     int sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -18,25 +39,10 @@ int main() {
 
     connect(sock, (sockaddr*)&addr, sizeof(addr));
 
-    while(true){
-      std:: string msg;
-      std::getline(std::cin,msg);
-      if(msg.empty()){
-      continue;
-      }
-      if(msg=="exit"){
-       break;
-      }
-      send(sock, msg.c_str(),msg.size(), 0);
- 
-     char buffer[1025] ;
-
-    int n=recv(sock, buffer, 1024, 0);
-    if(n<=0)break;
-    buffer[n] = '\0';
-    std::cout << "server: " << buffer << std::endl;
-}
+    std::thread sender(send_messages,sock);
+    std::thread receiver(recv_messages,sock);
+    sender.join();
+    receiver.join();
     close(sock);
-
     return 0;
 }
