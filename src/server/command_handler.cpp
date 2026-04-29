168b1ab7be9cf21986bcef7f42f22bd99fd6d315
jingyi <w121748@qq.com>
2026-04-29 21:16:31 +0800

    add chat commands for users help and rename

--- src/server/command_handler.cpp
+++ src/server/command_handler.cpp
@@ -28,11 +28,38 @@ bool handle_command(int client_fd, const std::string& msg) {
         std::string help_msg =
             "[" + get_current_time() + "] [system] Commands:\n"
             "/users - show online users\n"
-            "/help - show command help\n";
+            "/help - show command help\n"
+            "/rename <new_name> - change your name\n";
 
         send(client_fd, help_msg.c_str(), help_msg.size(), 0);
         return true;
     }
+    
+    const std::string rename_prefix = "/rename ";
+
+   if (msg.rfind(rename_prefix, 0) == 0) {
+    std::string new_name = msg.substr(rename_prefix.size());
+
+    if (new_name.empty()) {
+        std::string err =
+            "[" + get_current_time() + "] [system] Usage: /rename <new_name>\n";
+        send(client_fd, err.c_str(), err.size(), 0);
+        return true;
+    }
+
+    std::string old_name = get_client_name(client_fd);
+    set_client_name(client_fd, new_name);
+
+    std::string self_msg =
+        "[" + get_current_time() + "] [system] You changed your name to " + new_name + "\n";
+    send(client_fd, self_msg.c_str(), self_msg.size(), 0);
+
+    std::string notify_msg =
+        "[" + get_current_time() + "] [system] " + old_name + " changed name to " + new_name + "\n";
+    broadcast_msg(client_fd, notify_msg);
+
+    return true;
+ }
 
     if (!msg.empty() && msg[0] == '/') {
         std::string unknown_msg =
